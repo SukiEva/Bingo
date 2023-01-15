911b2d6dd1c21af8b53b2c78e59cad6a6bab1c05
GitHub Action <action@github.com>
2023-01-15 05:06:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/sukieva/bingo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sukieva/bingo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sukieva.bingo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sukieva.bingo.MyBundle
 
 class MyApplicationService {
 
